4a9d9f3ca66df166d3e6dec0ba2f5502204b6f83
Zesc <jeddak.cesc@gmail.com>
2018-07-06 00:04:43 -0700

    adding an object list to script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var bookList = {
+	{
+		id : 1,
+		price : 10.80,
+		quantity : 0,
+		bookCover : 'Product-Image-Coming-Soon'
+	},
+	{
+		id : 2,
+		price : 15.68,
+		quantity : 0,
+		bookTitle : 'The Mastery',
+		bookCover : 'Product-Image-Coming-Soon'
+	},
+	{
+		id : 3,
+		price : 13.95,
+		bookTitle : 'Think and Grow Rich',
+		bookCover : 'Product-Image-Coming-Soon'
+	},
+	{
+		id : 4,
+		price : 5.99,
+		bookTitle : 'As a Man Thinked', 
+		bookCover : 'Product-Image-Coming-Soon'
+	},
+	{
+		id : 4,
+		price : 13.52,
+		bookTitle : 'No Excuses: The Power of Self-Discipline',
+		bookCover : 'Product-Image-Coming-Soon'
+	}
+	{
+		id : 5,
+		price : 14.55,
+		bookTitle : 'The 10X Rule The Only...',
+		bookCover : 'Product-Image-Coming-Soon'
+	}
+	// continue building you data book list.
+};
